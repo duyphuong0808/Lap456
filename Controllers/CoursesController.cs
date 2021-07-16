87d8b1db050badcc34a32465f8e0c53185aa9314
Do Phuong <doduyphuong0808@gmail.com>
2021-07-16 22:14:03 +0700

    update- cancel

--- Controllers/CoursesController.cs
+++ Controllers/CoursesController.cs
@@ -23,7 +23,8 @@ namespace Lap456.Controllers
         {
             var viewModel = new CourseViewModel
             {
-                Categories = _dbContext.Categories.ToList()
+                Categories = _dbContext.Categories.ToList(),
+                Heading = "Add Course"
             };
             return View(viewModel);
         }
@@ -93,10 +94,28 @@ namespace Lap456.Controllers
                 Time = course.DateTime.ToString("HH:mm"),
                 Category = course.CategoryId,
                 Place = course.Place,
-                //Heading = "Edit Course",
-                //Id = course.Id
+                Heading = "Edit Course",
+                Id = course.Id
             };
             return View("Create", viewModel);
+        }
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Update(CourseViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                viewModel.Categories = _dbContext.Categories.ToList();
+                return View("Create", viewModel);
+            }
+            var userId = User.Identity.GetUserId();
+            var course = _dbContext.Courses.Single(c => c.Id == viewModel.Id && c.LecturerId == userId);
+            course.Place = viewModel.Place;
+            course.DateTime = viewModel.GetDateTime();
+            course.CategoryId = viewModel.Category;
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
